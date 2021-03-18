742316c2295a9c594a26d0aa207794181acc4fae
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-18 20:01:06 +0530

    Add files via upload

---
+++ Program2.c
@@ -0,0 +1,8 @@
+// a program in c to find the sum and mean of 4 numbers
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	printf("\n Sum is %d and avg is %d",(a+b+c+d),(a+b+c+d)/4);
+}
