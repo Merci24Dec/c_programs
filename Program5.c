d0a20e8e7a16ecf0501b7b16db18b206505b0b58
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-21 18:45:32 +0530

    Add files via upload

---
+++ Program5.c
@@ -0,0 +1,11 @@
+//accept two numbers and finding out the greatest one
+#include<stdio.h>
+void main()
+{
+int a,b;
+scanf("%d%d",&a,&b);
+if(a>b)
+printf("\n greater is%d",a);
+else 
+printf("\n greater is %d",b);
+}
