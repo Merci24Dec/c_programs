0e605b5750a42d96eab4c5e03433352ff844f6a0
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-22 08:07:53 +0530

    Add files via upload

---
+++ Program6.c
@@ -0,0 +1,14 @@
+//to accept a no and print whether it is a +ve no or not.
+#include<stdio.h>
+void main()
+{
+int num;
+scanf("%d",&num);
+if(num>=0)
+printf("\n it is a +ve number");
+else
+printf("\n it is a -ve number");
+}
+
+
+
