351c6d370cc0f2efef340ca6bbbc4649b670f1c3
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-16 20:15:51 +0530

    Add files via upload

---
+++ Program44.c
@@ -0,0 +1,18 @@
+//Print the following pattern:
+//a
+//a a
+//a a a
+//a a a a
+#include<stdio.h>
+void main()
+{
+int i,j;
+for(i=1;i<=4;i++)
+{
+printf("\n");
+for(j=1;j<=i;j++)
+{
+printf("a ");
+}
+}
+}
