e6cf34980f4d6a4d689e722bb08e6b9d452bd9f2
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-21 19:51:26 +0530

    Add files via upload

---
+++ Program58.c
@@ -0,0 +1,13 @@
+//math functions
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int m,n,p;
+printf("Enter the number\n");
+scanf("%d",&m);
+printf("Enter the power\n");
+scanf("%d",&n);
+p=pow(m,n);
+printf("Power of given number %d\n",p);
+}
