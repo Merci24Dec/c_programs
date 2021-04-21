394681855fc6a151d256ee1fb5867dcc522c6fa3
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-21 19:51:41 +0530

    Add files via upload

---
+++ Program59.c
@@ -0,0 +1,11 @@
+//math functions
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int m,n;
+printf("\n Enter a number\n");
+scanf("%d",&m);
+n=sqrt(m);
+printf("Square root of given number %d\n",n);
+}
