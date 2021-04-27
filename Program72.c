6c37d5561d9eb80dd8ddd2b305e8670138fd349f
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-27 21:52:04 +0530

    Add files via upload

---
+++ Program72.c
@@ -0,0 +1,18 @@
+//auto(storage class)
+int fun(int x);
+#include<stdio.h>
+void main()
+{
+int i,f;
+for(i=1;i<=10;i++)
+{
+f=fun(i);
+printf("\n %d | %d",i,f);
+}
+}
+int fun(int x)
+{
+auto int s = 100;
+s=s+x;
+return(s);
+}
