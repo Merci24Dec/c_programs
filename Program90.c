cf17fef40e0e79ab6f980fd332e2af0d47e37bb8
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-01 20:18:04 +0530

    Add files via upload

---
+++ Program90.c
@@ -0,0 +1,11 @@
+//String array starts from here
+//Accept a string and print it
+#include<stdio.h>
+void main()
+{
+char s[20];
+printf("\n Enter a string : ");
+gets(s);
+printf("\n Entered string is : ");
+puts(s);
+}
